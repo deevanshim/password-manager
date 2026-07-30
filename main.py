f2ca4ea7b8c5c8b7517e35ed4e581b7789e27a1d
Deevanshi <deevanhimalhotra@gmail.com>
2026-07-29 19:49:07 -0700

    Add password search functionality

--- main.py
+++ main.py
@@ -13,17 +13,30 @@ def view_passwords():
     if len(passwords) == 0:
         print("No passwords saved.")
         return 
-    for i in passwords:
+    for item in passwords:
         print("-----------")
-        print("Website:", i["website"])
-        print("Username:", i["username"])
-        print("Password:", i["password"])
+        print("Website:", item["website"])
+        print("Username:", item["username"])
+        print("Password:", item["password"])
+
+def search_passwords():
+    website_search = input("\n enter the website to search")
+    found = False
+    for item in passwords:
+        if item["website"].lower() == website_search.lower():
+            print("Website:", item["website"])
+            print("Username:", item["username"])
+            print("Password:", item["password"])
+            found = True
+        if found == False:
+            print("No password found.")
 def show_menu():
     print("""
     PASSWORD MANAGER
     1. Add Password
     2. View Passwords
-    3. Exit""")
+    3. Search Password
+    4. Exit""")
 while True:
     show_menu()
     choice = input("Choose option: ")
@@ -32,8 +45,10 @@ while True:
     elif choice == '2':
         view_passwords()
     elif choice == '3':
+        search_passwords()
+    elif choice == '4':
         print('Goodbye!')
         break
     else:
         print('Invalis Choice')
-        
+
